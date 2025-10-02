7a5ffc7bd47d2ba622f70369fd24c6ca89b08d33
Taitoos2 <carlos.barahonapas@gmail.com>
2025-10-02 11:29:10 +0200

    Add files via upload

---
+++ logistics.py
@@ -0,0 +1,145 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dataclasses import dataclass, field
+from scipy.interpolate import interp1d
+from scipy.linalg import expm
+from numpy.fft import fft, fftfreq
+from math import comb, factorial
+
+# ---------------- Heisenberg Langevin integrator ---------------------------- # 
+
+from scipy.integrate import DOP853
+s_0 = np.asarray([[0,0],[1,0]])
+
+@dataclass
+
+class new_cav_model:
+	""" This class is used as an integrator for an Two Level System (TLS) coupled to a " Long cavity" . The TLS  interacts with itself in 
+	a non-Markovian regime, and is coherently driven by a classical coherent field resonant with the frequency of the excited state. 
+	
+	Arguments
+	---------
+	gamma: float
+		Effective coupling between the emitters and the EM field
+	phi: float
+		Phase acquired by the photon when travelling the entire waveguide and back 
+	Omega: float 
+		Rabi frequency, fixes the intensity of the coherent field. 
+	eta: float 
+		The fraction of field that stays in the waveguide when bouncing back at the mirror. 
+		
+	buffer_size: int = 100
+		Dimension of buffer for saving operators
+
+	"""
+
+	gamma: float = 1.0
+	phi: float = np.pi
+	tau: float = 10
+	Omega: float = 0.0 
+	eta: float =1.0
+
+	buffer_size: int = 100
+	next_index: int = 0
+
+	s_array: np.ndarray = field(init=False)  	# list that stores values of sigma
+	a_out_array: np.ndarray = field(init=False)  # list that stores values of a_out(t)
+	t_array: np.ndarray = field(init=False)  	# list that stores values of t
+	
+	a_out_delayed: np.ndarray = field(init=False)  	# local variable to store the value of a_out(t-\tau)
+
+	a_dalayed : callable = field(init=False, default=None)  # interpolator 
+	interpolador_low_index : int = 0 
+	interpolador_high_index: int = 0 
+
+	def __post_init__(self):
+
+		self.Delta = self.phi / self.tau
+		self.next_index = 1
+		self.a_out_delayed=np.zeros((2,2),dtype=np.complex128).reshape(-1)
+		self.a_delayed = None
+
+		self.s_array = np.ones((self.buffer_size, 1),dtype=np.complex128) * s_0.reshape(-1)
+		self.a_out_array = np.ones((self.buffer_size,1), dtype=np.complex128) *np.sqrt(self.gamma)*s_0.reshape(-1)
+		self.t_array = np.zeros(self.buffer_size)
+
+
+	def derivative(self,t,y):
+		" To be Done: implement a mobile window of interpolation"
+		s=y.reshape((2,2))
+		sz = 2*(np.conjugate(s.T))@s - np.eye(2)
+		dsdt = sz@(0.5*self.gamma*s -1j*self.Omega*0.5*np.eye(2))
+		if t > self.tau + 1e-12 :
+			interpolator=interp1d(self.t_array[:self.next_index],
+			self.a_out_array[:self.next_index],
+			assume_sorted=True,
+			axis=0,
+			copy=False,
+			)
+			self.a_out_delayed = interpolator(t-self.tau)
+		return (dsdt - np.sqrt(self.gamma)*np.exp(1j*(self.phi+np.pi))*sz@self.a_out_delayed.reshape((2,2))).reshape(-1)
+	
+	def evolve(self,t_max,dt):
+		integrator = DOP853(self.derivative,self.t_array[0],self.s_array[0],t_bound=t_max,max_step=dt)
+		
+		while integrator.t<t_max:
+			
+			integrator.step()
+			
+			if integrator.status == 'failed':
+				print("Integrador falló en i =", i)
+				i = self.next_index
+				if i >= self.buffer_size:
+					''' resize arrays when necessary '''
+					self.buffer_size = int(self.buffer_size * 1.5)
+					self.t_array = np.resize(self.t_array, self.buffer_size)
+					self.s_array = np.resize(self.s_array, (self.buffer_size, 4))
+					self.a_out_array = np.resize(self.a_out_array, (self.buffer_size, 4))
+
+				self.t_array = self.t_array[:i] # in case the integration fails, I trim the arrays 
+				self.s_array = self.s_array[:i]
+				self.a_out_array = self.a_out_array[:i]  
+			#	print(integrator.status + ' at: '+str(integrator.t))
+			#	print(self.next_index)
+			
+			i = self.next_index
+			if i >= self.buffer_size:
+				''' resize arrays when necessary '''
+				self.buffer_size = int(self.buffer_size * 1.5)
+				self.t_array = np.resize(self.t_array, self.buffer_size)
+				self.s_array = np.resize(self.s_array, (self.buffer_size, 4))
+				self.a_out_array = np.resize(self.a_out_array, (self.buffer_size, 4))
+
+			self.t_array[i] = integrator.t
+			self.s_array[i] = integrator.y
+			self.a_out_array[i] = np.sqrt(self.gamma)*integrator.y +self.eta*np.exp(1j*self.phi)*self.a_out_delayed 
+			self.next_index = i + 1	
+
+
+		self.t_array = self.t_array[:self.next_index] # trim the array 
+		self.s_array = self.s_array[:self.next_index]
+		self.a_out_array = self.a_out_array[:self.next_index]
+
+	def excited_state(self,initial):
+		s = self.s_array.reshape(-1,2,2)
+		s_dag = np.conjugate(np.transpose(s,axes=(0,2,1)))
+		return self.t_array,np.abs(np.einsum('i,tik,tkj,j->t',np.conjugate(np.asarray(initial).T),s_dag,s,np.asarray(initial)))
+
+# ----------------- analytical solution single emitter ----------------------- 
+
+from numpy.polynomial import Polynomial
+
+def J_analytical_new(gamma,phi,tau,t):
+    
+    alpha = 1j*phi/tau + 0.5*gamma 
+    result =  np.exp(-alpha*t)*np.ones(len(t),dtype=complex)
+    poli = Polynomial([1])
+    N = int(t[-1]/tau)
+    
+    for n in range(1,N+1):
+        dummie = poli.integ() 
+        result += np.exp(-alpha*t)*np.exp(n*alpha*tau)*dummie(-gamma*(t-n*tau))*np.heaviside(t-n*tau,1)
+        poli += dummie 
+
+        
+    return result
---
+++ logistics_exact_cavity.py
@@ -0,0 +1,124 @@
+import numpy as np 
+from dataclasses import dataclass, field
+from scipy.linalg import expm
+
+# -------------------------- exact one-mode cavity integrator ------------------------------ 
+
+@dataclass
+class Driven_cavity_class: 
+	omega_qubit : float = 1 
+	omega_laser : float = 1			# laser initialy resonant with the qubit
+	omega_cavity : float= 1	  		# cavity initialy resonant with the qubit
+	Driving_intensity : float = 0.0	
+	g  : float= 0.5 
+	N_photons : int = 20 # Max number of excitations in the cavity - 1 
+
+	sigma_minus : np.ndarray = field(init=False) 
+	sigma_plus : np.ndarray = field(init=False) 
+	b : np.ndarray = field(init=False)
+	b_dag : np.ndarray = field(init=False) 
+	Hamiltonian_0: np.ndarray = field(init=False) 
+	psi0: np.ndarray = field(init=False) 
+	
+	 
+	def __post_init__(self):
+		B = np.zeros((self.N_photons,self.N_photons))
+		B_dag = np.zeros((self.N_photons,self.N_photons))
+		for n in range(self.N_photons-1):
+			B[n,n+1] = np.sqrt(n+1)
+			B_dag[n+1,n] = np.sqrt(n+1)
+			
+		self.sigma_plus = np.kron(np.array([[0,1],[0,0]]),np.eye(self.N_photons))
+		self.sigma_minus = np.kron(np.array([[0,0],[1,0]]),np.eye(self.N_photons))
+		self.b = np.kron(np.eye(2),B)
+		self.b_dag = np.kron(np.eye(2),B_dag)
+
+		self.Hamiltonian_0 = (self.omega_qubit-self.omega_laser)*self.sigma_plus@self.sigma_minus + (self.omega_cavity-self.omega_laser)*self.b_dag@self.b 
+		self.Hamiltonian_0 +=  self.g*(self.sigma_plus@self.b + self.sigma_minus@self.b_dag) 
+		self.Hamiltonian_0 += self.Driving_intensity/2 * (self.sigma_minus + self.sigma_plus)
+
+		initial_cav = np.zeros(self.N_photons,dtype=complex)   
+		initial_cav[0]=1 # zero photons in the initial state 
+		initial_emitter = np.array([1,0],dtype=complex) # one excitation in the initial state  
+		self.psi0 = np.kron(initial_emitter,initial_cav)
+
+
+
+	def evolve(self,times):
+		vec = []
+		for t in times:
+			U = expm(-1j*self.Hamiltonian_0*t)
+			psi_t = U @ self.psi0
+			vec.append(psi_t)
+		vec = np.array(vec)
+		pe = np.real(np.einsum('ti,ij,tj ->t ',np.conjugate(vec),self.sigma_plus@self.sigma_minus,vec))
+		exc =  np.real(np.einsum('ti,ij,tj ->t ',np.conjugate(vec),self.b_dag@self.b,vec))
+		return pe,exc
+	
+# --------------------- exact two-mode cavity integrator ----------------------------------------- 
+
+@dataclass
+
+class two_cavities_qubit: 
+	''' To be done: this class can substitute the previous one (Driven_cavity_class),
+	but there is no need to delete anything so far.  '''
+	omega_q : float = 1 
+	omega_c1:float = 1.2 
+	omega_c2 : float = 1.8 
+	g1:float = 0.2 
+	g2: float = 0.2
+	Rabi_freq : float  = 0.0 
+	driving_freq: float = 1 
+	N_photons : int = 10 # Max number of excitations in the cavity - 1 
+
+	sigma_minus : np.ndarray = field(init=False) 
+	sigma_plus : np.ndarray = field(init=False) 
+	b1 : np.ndarray = field(init=False)
+	b1_dag : np.ndarray = field(init=False) 
+	b2 : np.ndarray = field(init=False)
+	b2_dag : np.ndarray = field(init=False) 
+	Hamiltonian_0: np.ndarray = field(init=False) 
+	psi0: np.ndarray = field(init=False) 
+	
+	 
+	def __post_init__(self):
+		B = np.zeros((self.N_photons,self.N_photons))
+		B_dag = np.zeros((self.N_photons,self.N_photons))
+		for n in range(self.N_photons-1):
+			B[n,n+1] = np.sqrt(n+1)
+			B_dag[n+1,n] = np.sqrt(n+1)
+			
+		self.sigma_plus = np.kron(np.kron(np.array([[0,1],[0,0]]),np.eye(self.N_photons)),np.eye(self.N_photons))
+		self.sigma_minus = np.kron(np.kron(np.array([[0,0],[1,0]]),np.eye(self.N_photons)),np.eye(self.N_photons))
+		self.b1 = np.kron(np.kron(np.eye(2),B),np.eye(self.N_photons))
+		self.b1_dag = np.kron(np.kron(np.eye(2),B_dag),np.eye(self.N_photons))
+		self.b2 = np.kron(np.kron(np.eye(2),np.eye(self.N_photons)),B)
+		self.b2_dag = np.kron(np.kron(np.eye(2),np.eye(self.N_photons)),B_dag)
+
+		self.Hamiltonian_0 = (self.omega_q-self.driving_freq)*self.sigma_plus@self.sigma_minus 
+		self.Hamiltonian_0 += (self.omega_c1-self.driving_freq)*self.b1_dag@self.b1
+		self.Hamiltonian_0 +=  (self.omega_c2-self.driving_freq)*self.b2_dag@self.b2
+		self.Hamiltonian_0 +=  self.g1*(self.sigma_plus@self.b1 + self.sigma_minus@self.b1_dag) 
+		self.Hamiltonian_0 +=  self.g2*(self.sigma_plus@self.b2 + self.sigma_minus@self.b2_dag) 
+		self.Hamiltonian_0 += 0.5*self.Rabi_freq * (self.sigma_minus + self.sigma_plus)
+
+		initial_c1 = np.zeros(self.N_photons,dtype=complex)   
+		initial_c1[0]=1 # zero photons in the initial state 
+		initial_emitter = np.array([1,0],dtype=complex) # one excitation in the initial state  
+		self.psi0 = np.kron(initial_emitter,initial_c1)
+		self.psi0 = np.kron(self.psi0,initial_c1)
+
+
+
+	def evolve(self,times):
+		vec = []
+		for t in times:
+			U = expm(-1j*self.Hamiltonian_0*t)
+			psi_t = U @ self.psi0
+			vec.append(psi_t)
+		vec = np.array(vec)
+		pe = np.real(np.einsum('ti,ij,tj ->t ',np.conjugate(vec),self.sigma_plus@self.sigma_minus,vec))
+		exc1 =  np.real(np.einsum('ti,ij,tj ->t ',np.conjugate(vec),self.b1_dag@self.b1,vec))
+		exc2 =  np.real(np.einsum('ti,ij,tj ->t ',np.conjugate(vec),self.b2_dag@self.b2,vec))
+		return pe,exc1,exc2
+	
